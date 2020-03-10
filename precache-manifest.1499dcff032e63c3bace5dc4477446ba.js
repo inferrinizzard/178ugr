05bc1b621c06eb7738f3240786b342bf57bb7517
inferrinizzard <inferrinizzard@gmail.com>
2020-03-10 09:47:31 -0700

    Updates

---
+++ precache-manifest.1499dcff032e63c3bace5dc4477446ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b2c2eb45ad53e258896ee1dfad46114",
+    "url": "/178ugr/index.html"
+  },
+  {
+    "revision": "c6b7fd5eabda5d735b12",
+    "url": "/178ugr/static/css/main.2dc8001a.chunk.css"
+  },
+  {
+    "revision": "76113c73659c8e750cb6",
+    "url": "/178ugr/static/js/2.ca23fc83.chunk.js"
+  },
+  {
+    "revision": "4214db9a6c23cd334176e62fe82579f5",
+    "url": "/178ugr/static/js/2.ca23fc83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6b7fd5eabda5d735b12",
+    "url": "/178ugr/static/js/main.f4eae54e.chunk.js"
+  },
+  {
+    "revision": "9422a5b07cc534a72942",
+    "url": "/178ugr/static/js/runtime-main.de41924e.js"
+  }
+]);
